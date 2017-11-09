6590fa8e91ac0c1b9c9f9a62a56b5d8f1dec00b2
Linjliang <864483116@qq.com>
2017-11-10 00:09:11 +0800

    MainActivity modified by gy

--- app/src/main/java/com/gy/linjliang/nationsapp/MainActivity.java
+++ app/src/main/java/com/gy/linjliang/nationsapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.gy.linjliang.nationsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +21,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Button buttonwei=(Button) findViewById(R.id.buttonwei);
+        final Button buttonshu=(Button) findViewById(R.id.buttonshu);
+        final Button buttonwu=(Button) findViewById(R.id.buttonwu);
 
+        buttonwei.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mes=new Intent(MainActivity.this,InfoList.class);
+                mes.putExtra("countryname","魏");
+                startActivity(mes);
+
+            }
+        });
+
+        buttonshu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mes=new Intent(MainActivity.this,InfoList.class);
+                mes.putExtra("countryname","蜀");
+                startActivity(mes);
+            }
+        });
+
+        buttonwu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mes=new Intent(MainActivity.this,InfoList.class);
+                mes.putExtra("countryname","吴");
+                startActivity(mes);
+            }
+        });
     }
 }
